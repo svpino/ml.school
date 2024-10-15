822d5ebb82bfd00fb26b2a6472b47e5febc1b45e
svpino <svpino@gmail.com>
2024-10-15 20:44:11 +0000

    idx

--- idx-template.nix
+++ idx-template.nix
@@ -7,15 +7,14 @@
     # of the checked-out Git folder containing this template.
     cp -rf ${./.} "$out"
 
+    chmod -R +w "$out"
+
     cat << EOF >> "$out/.aws"
     [default]
     aws_access_key_id = ${aws_access_key_id}
     aws_secret_access_key = ${aws_secret_access_key}
     aws_region = ${aws_region}
     EOF
-
-    # Set some permissions
-    chmod -R +w "$out"
 
     # Remove the template files
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
