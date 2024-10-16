4c6b0c89e71cbdac6c02a1484968354b9491fc3e
svpino <svpino@gmail.com>
2024-10-16 19:07:23 +0000

    ...

--- idx-template.nix
+++ idx-template.nix
@@ -8,11 +8,13 @@
     cp -rf ${./.} "$out"
     chmod -R +w "$out"
 
+    touch "$out"/.idx/aws.nix
+
     cat << EOF >> "$out"/.idx/aws.nix
     {
-      AWS_ACCESS_KEY_ID = ${aws_access_key_id};
-      AWS_SECRET_ACCESS_KEY = ${aws_secret_access_key};
-      AWS_REGION = ${aws_region};
+      AWS_ACCESS_KEY_ID = "${aws_access_key_id};"
+      AWS_SECRET_ACCESS_KEY = "${aws_secret_access_key};"
+      AWS_REGION = "${aws_region};"
     }
     EOF
 
