4aeef43d03c501ff9a9f62ac640659b6927cead3
Santiago L. Valdarrama <svpino@gmail.com>
2024-10-25 08:48:08 -0400

    Upgrade pip before installing requirements

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -38,8 +38,7 @@ in
         python-venv = ''
           python3 -m venv /.venv
           source /.venv/bin/activate
-          pip install --upgrade pip
-          pip install -r requirements.txt
+          pip install -U pip && pip install -r requirements.txt
         '';
 
         environment = ''
