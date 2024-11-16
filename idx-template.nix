ccebe90d21ee603bb2ed418fe83e5d8c45f13a1c
Tanuj Mittal <65970009+mittaltanuj@users.noreply.github.com>
2024-11-16 19:06:46 +0530

    Testing to check if still blank is saved in .inx file

--- idx-template.nix
+++ idx-template.nix
@@ -1,13 +1,4 @@
-{ pkgs, aws_access_key_id ? "", aws_secret_access_key ? "", aws_region ? "", ... }: 
-let
-  default_aws_access_key_id = "";
-  default_aws_secret_access_key = "";
-  default_aws_region = "";
-  
-  final_aws_access_key_id = if aws_access_key_id == "" then default_aws_access_key_id else aws_access_key_id;
-  final_aws_secret_access_key = if aws_secret_access_key == "" then default_aws_secret_access_key else aws_secret_access_key;
-  final_aws_region = if aws_region == "" then default_aws_region else aws_region;
-in {
+{ pkgs, aws_access_key_id ? "", aws_secret_access_key ? "", aws_region ? "", ... }: {
   channel = "stable-23.11";
 
   bootstrap = ''
@@ -17,15 +8,15 @@ in {
     cp -rf ${./.} "$out"
     chmod -R +w "$out"
 
-    ${if final_aws_access_key_id != "" && final_aws_secret_access_key != "" && final_aws_region != "" then ''
+    ${if aws_access_key_id != "" && aws_secret_access_key != "" && aws_region != "" then ''
       cat << EOF >> "$out"/.idx/aws.nix
       {
-        AWS_ACCESS_KEY_ID = "${final_aws_access_key_id}";
-        AWS_SECRET_ACCESS_KEY = "${final_aws_secret_access_key}";
-        AWS_REGION = "${final_aws_region}";
+        AWS_ACCESS_KEY_ID = "${aws_access_key_id}";
+        AWS_SECRET_ACCESS_KEY = "${aws_secret_access_key}";
+        AWS_REGION = "${aws_region}";
       }
       EOF
-    '' else "echo "{}" >> \"$out\"/.idx/aws.nix"
+    '' else ''"echo "{}" >> \"$out\"/.idx/aws.nix"''
     }
 
     # Remove the template files
