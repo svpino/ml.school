07e1f42a6b55d39ffe3e428fd69d1d338743e7d4
Tanuj Mittal <65970009+mittaltanuj@users.noreply.github.com>
2024-11-16 19:16:50 +0530

    Testing to check if still blank is saved in .inx file - Cleaner version

--- idx-template.nix
+++ idx-template.nix
@@ -8,7 +8,8 @@
     cp -rf ${./.} "$out"
     chmod -R +w "$out"
 
-    ${if aws_access_key_id != "" && aws_secret_access_key != "" && aws_region != "" then ''
+    ${
+      ''
       cat << EOF >> "$out"/.idx/aws.nix
       {
         AWS_ACCESS_KEY_ID = "${aws_access_key_id}";
@@ -16,11 +17,7 @@
         AWS_REGION = "${aws_region}";
       }
       EOF
-    '' else ''
-      cat << EOF >> "$out"/.idx/aws.nix
-      {}
-      EOF
-    ''
+      ''
     }
 
     # Remove the template files
