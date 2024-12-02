f1aba2df841c454e5bd71f6280de1d86cb04a657
Santiago L. Valdarrama <svpino@gmail.com>
2024-12-02 16:11:41 -0500

    ...

--- .guide/introduction-to-metaflow/cards.py
+++ .guide/introduction-to-metaflow/cards.py
@@ -2,21 +2,36 @@ from metaflow import FlowSpec, card, step
 
 
 class Cards(FlowSpec):
-    """A flow that showcases how to use request computing resources."""
+    """A flow that showcases how to use cards to visualize results."""
 
     @card
     @step
     def start(self):
-        """Compute the dimensions of the matrix."""
+        """Initialize a few artifacts."""
         self.orange = 1
         self.apple = 2
 
+        self.next(self.report)
+
+    @card(type="html")
+    @step
+    def report(self):
+        """Generate a custom card."""
+        self.html = f"""
+        <h1>Custom Metaflow Card</h1>
+        <p style="background: orange; padding: 8px; width: 300px;">
+            Orange: {self.orange}
+        </p>
+        <p style="background: red; padding: 8px; width: 300px;">
+            Apple: {self.apple}
+        </p>
+        """
         self.next(self.end)
 
     @card
     @step
     def end(self):
-        """Print the final value."""
+        """Compute new artifact."""
         self.juice = self.orange + self.apple
 
 
