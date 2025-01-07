99e0d07e05e0b03fb5ba17e95c0164ad06f2dfef
Santiago L. Valdarrama <svpino@gmail.com>
2025-01-07 15:37:27 -0500

    refactor test to use a variable for cross-validation folds

--- tests/test_training_cross_validation.py
+++ tests/test_training_cross_validation.py
@@ -1,3 +1,4 @@
 def test_cross_validation_creates_5_folds(training_run):
     data = training_run["cross_validation"].task.data
-    assert len(data.folds) == 5
+    cross_validation_folds = 5
+    assert len(data.folds) == cross_validation_folds
